db232ce057a3299492ef7b771b7ce786a2845333
nero433 <albertnero29@gmail.com>
2025-01-22 13:49:05 +0000

    submit

--- python_practise/next_year.py
+++ python_practise/next_year.py
@@ -2,4 +2,4 @@ print("Hello, World!")
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 print(f"Hello {name}! You are {age} years old.") #returning the value of the name and age
-print(f"Next year you will be {age+1} years old.")
+print(f"Next year you will be {age+1} years old.") #returning the value of the age added by 1
